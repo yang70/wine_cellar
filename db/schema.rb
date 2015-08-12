b1f463ebab8c384cb3a680992288c166d15adfd4
Matthew Yang <yang70@gmail.com>
2015-08-12 15:18:42 -0700

    add create users framework

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150801220657) do
+ActiveRecord::Schema.define(version: 20150812213012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "wines", force: :cascade do |t|
     t.string   "name"
