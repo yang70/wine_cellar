71d16800ddbc1780d8e33b423457203b860b04a9
Matthew Yang <yang70@gmail.com>
2015-08-14 14:44:24 -0700

    update show-pic class

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -5,7 +5,7 @@ class Wine < ActiveRecord::Base
   has_attached_file :wine_pic, styles: {
     thumb: '100x100>',
     square: '200x200#',
-    medium: '300x',
+    medium: '300x300>'
   }
 
   validates_attachment_content_type :wine_pic, :content_type => /\Aimage\/.*\Z/
