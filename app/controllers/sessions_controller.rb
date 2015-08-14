b1ed324a8b0633cad107d541f76a333455ac67f1
Matthew Yang <yang70@gmail.com>
2015-08-14 15:21:09 -0700

    redirect to wines index on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to '/wines'
     else
       redirect_to 'login'
     end
